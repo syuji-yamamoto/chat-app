ac8b9dd28a821abbb6a968e955d73bbafc7487f8
syuji_yamamoto <51443836+syuji-yamamoto@users.noreply.github.com>
2021-04-28 13:20:50 +0900

    モデルの単体テスト

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'チャットルームの新規作成' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it 'nameの値が存在すれば登録できること' do
+      expect(@room).to be_valid
+    end
+
+    it 'nameが空では登録できないこと' do
+      @room.name = ""
+      @room.valid?
+      expect(@room.errors.full_messages).to include "Name can't be blank"
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe '#create' do
+  describe 'ユーザーの新規登録' do
     before do
       @user = FactoryBot.build(:user)
     end
